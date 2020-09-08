5478c0f7f0ba2aba115f271716adfacbd15cb745
ashbabkhan <47265812+ashbabkhan@users.noreply.github.com>
2020-09-07 23:05:10 -0700

    qa

---
+++ About.php
@@ -0,0 +1,13 @@
+<?php include 'navbar.php' ?>
+<div class="container">
+   <h1 class="display-2 text-center">About Us</h1>
+   <img src="prav.jpg">
+   <h1 class="display-2 text-center">Contact Us</h1>
+   <h4 class="text-center">Phone No:- 7248696199   </h4>
+   <h4 class="text-center">Email:-default@gmail.com</h4>
+   <h1 class="display-2 text-center">Address</h1>
+   <h4 class="text-center">Ch. Mahindra Pradhan Market,Near Bakson College</h4>
+   <h4 class="text-center">Tugalpur,Greater Noida,G.B Nagar </h4>
+</div>    
+
+<?php include 'footer.php' ?>
